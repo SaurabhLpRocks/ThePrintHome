3b983f1a1d019decc280e684e4cccf3b3223f349
Saurabh Palatkar <Saurabh Palatkar>
2016-01-16 19:42:50 +0530

    deleted project

--- App/config/routes/api.js
+++
@@ -1,9 +0,0 @@
-// 
-// var mongoose =require("mongoose");
-// var Post = mongoose.model('Post');
-// 
-// var router = require('./init');
-// var posts = require('./posts');
-// var posts = require('./user');
-// 
-// module.exports = router;
--- App/gulp.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-	var config = {
-		allTs:'./app/**/*.ts',
-		typeings:'./typings/**/*.ts',
-		tsOutPutPath:'./app'
-	};
-	return config;
-}
